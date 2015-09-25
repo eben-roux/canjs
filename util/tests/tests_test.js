94c2009e2984c9ac31b8af5e6f7fbf463800a75a
Mark Stahl <mjstahl@gmail.com>
2015-09-25 12:06:34 -0400

    Fixes #1938. Check for isArray, if not use toString method

--- util/tests/tests_test.js
+++ util/tests/tests_test.js
@@ -1,1 +1,1 @@
-steal('../string/string_test',  '../inserted/inserted_test', '../attr/attr_test', '../array/each_test');
+steal('../string/string_test',  '../inserted/inserted_test', '../attr/attr_test', '../array/each_test', '../domless/domless_test');
